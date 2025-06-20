930f9c89fb0e41c37e1db54bef71bd36844bc05b
aufa20 <0221123290@student.kuips.edu.my>
2025-06-21 07:02:35 +0800

    Fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
+import os  # Required for os.environ
 
 # Load model
 with open("genre_model.pkl", "rb") as f:
@@ -17,4 +18,7 @@ def predict():
     return jsonify({'genre': pred})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))
+    )
